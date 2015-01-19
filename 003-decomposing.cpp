c9fb83b48dc1e018c23558f7ee37d3ba8043b89f
Adrian Enns <addis.aden@gmail.com>
2015-01-19 23:24:07 +0100

    start example 4

---
+++ 003-decomposing.cpp
@@ -0,0 +1,10 @@
+/*
+ * https://developers.google.com/edu/c++/getting-started
+ *
+ * Example 4: Decomposing makes everything easier...
+ *
+ * [   ] Step 1
+ * [   ] Step 2
+ * [   ] Step 3
+ *
+ */
