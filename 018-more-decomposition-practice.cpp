d96a4d54d9e6e712f4b124b01ac3a25e8d89a2d4
Adrian Enns <addis.aden@gmail.com>
2015-01-20 23:52:47 +0100

    setup of artillery game works.

--- 018-more-decomposition-practice.cpp
+++ 018-more-decomposition-practice.cpp
@@ -7,3 +7,86 @@
  *
  */
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+
+// this is 200 ft/s or 60.96 m/s
+#define velocity 60.96
+
+// this is 32.2 ft/s or 9.81456 m/s
+#define gravity 9.81456
+
+// angle can be 0 - 360
+float angle_sin(float angle) { return sin(angle * M_PI / 180.0); }
+float angle_cos(float angle) { return cos(angle * M_PI / 180.0); }
+
+// funciton predecleration
+void introduction();
+void outro(int enemies_down);
+int give_a_shot();
+
+
+
+int main() {
+  introduction();
+
+  int enemies_down = 0;
+  string input;
+
+  do {
+    enemies_down += give_a_shot();
+    cout << endl << "You've killed " << enemies_down << (enemies_down == 1 ? " enemy." : " enemies.") << endl << endl;
+    cout << "Try again? (y/n) ";
+  } while((cin >> input), input == "y");
+
+  outro(enemies_down);
+
+  return 0;
+}
+
+
+
+void introduction() {
+  cout
+    << endl
+    << " ------------------------" << endl
+    << "-- Welcome to Artillery --" << endl
+    << " ------------------------" << endl
+    << endl
+    << "You are in the middle of a war and being charged by thousands of enemies. " << endl
+    << "You have one cannon, which you can shoot at any angle (0 - 90). " << endl
+    << endl;
+}
+
+void outro(int enemies_down) {
+  cout
+    << endl
+    << " ----------------------------" << endl
+    << "-- You killed " << setw(5) << enemies_down << " enemies --" << endl
+    << " ----------------------------" << endl;
+}
+
+
+
+// shoot-the-enemy game
+int give_a_shot() {
+  int cannon_balls = 10;
+  float angle_input;
+
+  do {
+    // shoot
+
+    cannon_balls--;
+
+    if(cannon_balls > 0)
+      cout << "You have " << cannon_balls << " cannonballs left." << endl;
+    else
+      cout << "You missed the enemy. He escaped." << endl;
+  } while(cannon_balls > 0);
+
+  return 0;
+}
--- cpp.vim
+++ cpp.vim
@@ -1,4 +1,4 @@
-map <F5> :!g++ -o %.out %<CR>
+map <F5> :!g++ -std=c++11 -o %.out %<CR>
 map <F6> :!./%.out<CR>
 
 echo "After Google you should learn http://www.glprogramming.com/red/"
