b77d6c717fec3bd49d4bd1297e14f8197baa60bc
Adrian Enns <addis.aden@gmail.com>
2015-01-20 13:09:28 +0100

    simple makefile testproject added

---
+++ tmp_002_testproject/hw.cpp
@@ -0,0 +1,7 @@
+#include "hw.h"
+#include <iostream>
+using namespace std;
+
+void hallo(string name) {
+  cout << "Hallo, " << name << "!" << endl;
+}
---
+++ tmp_002_testproject/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hw.h"
+using namespace std;
+
+int main() {
+  string str = "Welt";
+
+  hallo(str);
+
+  return 0;
+}
