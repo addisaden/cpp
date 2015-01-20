3b586138302b5d5118b25fc5f487f7507409f039
Adrian Enns <addis.aden@gmail.com>
2015-01-20 17:42:22 +0100

    processing files solved

--- 010-processing-files.cpp
+++ 010-processing-files.cpp
@@ -3,7 +3,55 @@
  *
  * Example 9: Processing Files
  *
- * [   ] done
+ * [ x ] done
  * 
  */
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#define filename "010-processing-files.txt.out"
+
+void list_entries() {
+  string vorname, nachname, telefonnummer;
+
+  // list current records
+  ifstream inpFile(filename, ifstream::in);
+  if(inpFile.is_open()) {
+    while(getline(inpFile, vorname) && getline(inpFile, nachname) && getline(inpFile, telefonnummer)) {
+      cout << ":: " << nachname << ", " << vorname << " (tel " << telefonnummer << ")" << endl;
+      inpFile.ignore(100, '\n');
+    }
+    inpFile.close();
+  }
+}
+
+int main() {
+  string vorname, nachname, telefonnummer, eingabe;
+
+  cout << endl;
+  cout << " ---------------" << endl;
+  cout << "-- Telefonbuch --" << endl;
+  cout << " ---------------" << endl << endl;
+
+  list_entries();
+
+  while(cout << endl << "Neuer Eintrag? (y/n) ", getline(cin, eingabe), eingabe == "y") {
+    ofstream outpFile(filename, ofstream::app);
+    if(outpFile.is_open()) {
+      cout << endl;
+      cout << "Vorname: ";
+      getline(cin, vorname);
+      cout << "Nachname: ";
+      getline(cin, nachname);
+      cout << "Telefon: ";
+      getline(cin, telefonnummer);
+      outpFile << vorname << endl << nachname << endl << telefonnummer << endl << endl;
+      outpFile.close();
+      cout << endl << "Eintrag hinzugefügt" << endl << endl;
+
+      list_entries();
+    }
+  }
+}
