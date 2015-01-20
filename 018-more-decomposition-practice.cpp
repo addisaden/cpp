852e777ae05ba96b46d33986b1458786f086eb71
Adrian Enns <addis.aden@gmail.com>
2015-01-20 22:32:13 +0100

    jump to next chapter (make later the exercises of getting-started

---
+++ 018-more-decomposition-practice.cpp
@@ -0,0 +1,9 @@
+/*
+ * https://developers.google.com/edu/c++/next-steps
+ *
+ * Example #1: More Decomposition Practice
+ *
+ * [   ] done
+ *
+ */
+
--- cpp.vim
+++ cpp.vim
@@ -1,2 +1,4 @@
 map <F5> :!g++ -o %.out %<CR>
 map <F6> :!./%.out<CR>
+
+echo "After Google you should learn http://www.glprogramming.com/red/"
