e96d62477f0d37e929858f48222a28af60d6345d
Adrian Enns <addis.aden@gmail.com>
2015-01-22 00:00:05 +0100

    better balancing ?

--- tmp_004_game_of_war/base.cpp
+++ tmp_004_game_of_war/base.cpp
@@ -3,52 +3,67 @@
 #include <cstdlib>
 #include <ctime>
 
-#define kSoldierBuild 3
-#define kSoldierBuildOptional 17
-#define kJeepBuild 3
-#define kJeepBuildOptional 4
-#define kTankBuild 0
-#define kTankBuildOptional 1
+/*         |  B | BOpt | Range | Hit  S, Cn,  Cz | Hit J,  Cn,  Cz | Hit T, Cn, Cz
+ * Soldier | 80 |  160 |     1 |20:  75,  4,  30 | 4: 18,  18, 162 |10: 40, 10, 80
+ * Jeep    | 10 |   20 |     4 |35:  50,  7,  20 |16: 24,   6,  18 | 2:  8,  6, 48
+ * Tank    |  2 |    4 |    10 |25: 125,  8,  80 |20: 60,   3,  18 | 8: 16,  2,  8
+ *
+ * SoldierCountBalance (SCB) = (40 +  80/2) *  1 = 80 * 4
+ * JeepCountBalance    (JCB) = ( 5 +  10/2) *  4 = 40 * 4
+ * TankCountBalance    (TCB) = ( 1 +   2/2) * 10 = 20 * 4
+ *
+ * SoldierHit = 80 = SP + JP + TP = 20 + 35 + 25 ) / 10
+ * JeepHit    = 40 = SP + JP + TP =  4 + 16 + 20 ) / 10
+ * TankHit    = 20 = SP + JP + TP = 10 +  2 +  8 ) / 10
+ *
+ */
+
+#define kSoldierBuild 80
+#define kSoldierBuildOptional 160
+#define kJeepBuild 10
+#define kJeepBuildOptional 20
+#define kTankBuild 2
+#define kTankBuildOptional 4
 
 #define kSoldierRadius 1
-#define kJeepRadius 3
+#define kJeepRadius 4
 #define kTankRadius 10
 
-#define kSoldierHitSoldier 7
-#define kSoldierHitSoldierChanceN 1
-#define kSoldierHitSoldierChanceZ 20
+#define kSoldierHitSoldier 75
+#define kSoldierHitSoldierChanceN 4
+#define kSoldierHitSoldierChanceZ 150
 
-#define kSoldierHitJeep 2
-#define kSoldierHitJeepChanceN 5
-#define kSoldierHitJeepChanceZ 120
+#define kSoldierHitJeep 18
+#define kSoldierHitJeepChanceN 18
+#define kSoldierHitJeepChanceZ 810
 
-#define kSoldierHitTank 1
-#define kSoldierHitTankChanceN 1
-#define kSoldierHitTankChanceZ 50
+#define kSoldierHitTank 40
+#define kSoldierHitTankChanceN 10
+#define kSoldierHitTankChanceZ 400
 
-#define kJeepHitSoldier 40
-#define kJeepHitSoldierChanceN 2
-#define kJeepHitSoldierChanceZ 30
+#define kJeepHitSoldier 50
+#define kJeepHitSoldierChanceN 7
+#define kJeepHitSoldierChanceZ 100
 
-#define kJeepHitJeep 5
-#define kJeepHitJeepChanceN 3
-#define kJeepHitJeepChanceZ 50
+#define kJeepHitJeep 24
+#define kJeepHitJeepChanceN 6
+#define kJeepHitJeepChanceZ 90
 
-#define kJeepHitTank 1
-#define kJeepHitTankChanceN 1
-#define kJeepHitTankChanceZ 30
+#define kJeepHitTank 8
+#define kJeepHitTankChanceN 6
+#define kJeepHitTankChanceZ 240
 
-#define kTankHitSoldier 5
-#define kTankHitSoldierChanceN 9
-#define kTankHitSoldierChanceZ 100
+#define kTankHitSoldier 125
+#define kTankHitSoldierChanceN 8
+#define kTankHitSoldierChanceZ 400
 
-#define kTankHitJeep 2
-#define kTankHitJeepChanceN 1
-#define kTankHitJeepChanceZ 80
+#define kTankHitJeep 60
+#define kTankHitJeepChanceN 3
+#define kTankHitJeepChanceZ 90
 
-#define kTankHitTank 2
-#define kTankHitTankChanceN 3
-#define kTankHitTankChanceZ 50
+#define kTankHitTank 16
+#define kTankHitTankChanceN 2
+#define kTankHitTankChanceZ 40
 
 GameOfWar::Base::Base() {
   srand(time(NULL));
